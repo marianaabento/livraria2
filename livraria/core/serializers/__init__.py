2a252ff83b1e5e3022ad7ebc3d2e4d05c53466ef
marianaabento <mariibentoo04@gmail.com>
2022-09-13 16:35:48 -0300

    chances

---
+++ livraria/core/serializers/__init__.py
@@ -0,0 +1,4 @@
+from .autor import AutorSerializers
+from .livro import LivroSerializers
+from .categoria import CategoriaSerializers
+from .editora import EditoraSerializers
---
+++ livraria/core/views/__init__.py
@@ -0,0 +1,4 @@
+from .autor import AutorViewSet
+from .categoria import CategoriaViewSet
+from .livro import LivroViewSet
+from .editora import EditoraViewSet
