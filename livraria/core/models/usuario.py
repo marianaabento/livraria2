a4bbc3af45c64d35c8ad1e94ba4d0516d9f57b6e
marianaabento <mariibentoo04@gmail.com>
2022-10-20 14:56:39 -0300

    retirada do cpf usuario no admin.py e usuario.py

--- livraria/core/models/usuario.py
+++ livraria/core/models/usuario.py
@@ -1,8 +1,9 @@
+from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class Usuario(AbstractUser):
-    cpf = models.CharField(max_length=11, unique=True)
+    nome = models.CharField(max_length=100, blank=True, null=True)
     telefone = models.CharField(max_length=11, blank=True, null=True)
     data_nascimento = models.DateField(blank=True, null=True)
