234f24012e6fe952a305d73b1e991b3ca30dcc3f
marianaabento <mariibentoo04@gmail.com>
2022-10-20 14:28:24 -0300

    testando as permissões

--- livraria/livraria/settings.py
+++ livraria/livraria/settings.py
@@ -125,8 +125,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
         "rest_framework.permissions.DjangoModelPermissions",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ]
+    )
 }
